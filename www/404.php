7220f98489d63ade72c98b3b1ce004462c0043eb
ZeyDB <zeydullah.eskenderov@gmail.com>
2019-10-23 15:03:44 +0300

    404

--- www/404.php
+++ www/404.php
@@ -2,6 +2,8 @@
 defined ('_DSITE') or die ('Access denied');
 header("HTTP/1.0 404 Not Found");
 ?>
+
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
 <div class="error404">
     <span class="error404">Что-то пошло не так</span>
     <strong class="error404">Ошибка 404</strong>
